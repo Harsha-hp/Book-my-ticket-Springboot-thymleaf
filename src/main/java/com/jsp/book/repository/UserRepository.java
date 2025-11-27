5a7686bf39f44adfa9583d98f8daad6d1e915637
hash5723 <harshithahp838@gmail.com>
2025-11-27 20:40:08 +0530

    Admin Registration

--- src/main/java/com/jsp/book/repository/UserRepository.java
+++ src/main/java/com/jsp/book/repository/UserRepository.java
@@ -3,5 +3,9 @@ package com.jsp.book.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.jsp.book.entity.User;
 public interface UserRepository extends JpaRepository<User, Long>{
+	
+	boolean existsByEmail(String email);
+
+	void deleteByRole(String string);
 
 }
--- src/main/java/com/jsp/book/util/EmailHelper.java
+++ src/main/java/com/jsp/book/util/EmailHelper.java
@@ -1,8 +1,5 @@
 package com.jsp.book.util;
 
-import org.springframework.stereotype.Component;
-
-@Component
 public class EmailHelper {
 
 }
